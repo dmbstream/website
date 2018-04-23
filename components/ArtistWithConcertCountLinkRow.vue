79008377ce2fe72b060ff151c4351f14544c1bff
Jim Geurts <jim@biacreations.com>
2018-04-22 19:10:32 -0500

    Hook up artists landing page

---
+++ components/ArtistWithConcertCountLinkRow.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <div class="col"><nuxt-link :to="{
+      name: 'artists-id-slug',
+      params: {
+        id: artist.id,
+        slug: slugify(artist.name),
+      },
+    }">{{ artist.name }} ({{ artist.concert_count }} concerts)</nuxt-link></div>
+  </div>
+</template>
+
+<script>
+  import stringService from '../plugins/stringService';
+
+  export default {
+    props: {
+      artist: {
+        type: Object,
+        required: true,
+      },
+    },
+    methods: {
+      slugify: stringService.slugify
+    },
+  };
+</script>
--- pages/artists/index.vue
+++ pages/artists/index.vue
@@ -1,11 +1,84 @@
 <template>
   <section class="content content-sidebar-right">
-    TODO!
+    <header class="content__title">
+      <h1>Artists</h1>
+    </header>
+
+    <div class="row">
+      <div class="col-md">
+        <div class="card">
+          <div class="card-header">
+            <h2 class="card-title">All Artists</h2>
+          </div>
+          <div class="card-block" v-if="allArtistsLoading">
+            Loading...
+          </div>
+          <div v-else>
+            <div class="card-block">
+              <template v-for="artist in allArtists">
+                <artist-with-concert-count-link-row :artist="artist" :key="artist.id"/>
+              </template>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md">
+        <div class="card">
+          <div class="card-header">
+            <h2 class="card-title">Most Popular Artists</h2>
+          </div>
+          <div class="card-block" v-if="popularArtistsLoading">
+            Loading...
+          </div>
+          <div v-else>
+            <div class="card-block">
+              <template v-for="artist in popularArtists">
+                <artist-with-concert-count-link-row :artist="artist" :key="artist.id"/>
+              </template>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
+  import ArtistWithConcertCountLinkRow from '../../components/ArtistWithConcertCountLinkRow';
+  import axios from '../../plugins/axios';
+  import sortBy from 'lodash/sortBy';
+
   export default {
-    scrollToTop: true
-  }
+    scrollToTop: true,
+    components: {
+      ArtistWithConcertCountLinkRow,
+    },
+    data() {
+      return {
+        popularArtistsLoading: true,
+        popularArtists: [],
+        allArtistsLoading: true,
+        allArtists: [],
+      };
+    },
+    async asyncData() {
+      const popularArtistResults = await axios.get('/api/artists', {
+        params: {
+          sortDesc: 'ConcertCount',
+        },
+      });
+
+      return {
+        popularArtistsLoading: false,
+        popularArtists: popularArtistResults.data.items,
+        allArtistsLoading: false,
+        allArtists: sortBy(popularArtistResults.data.items, 'name'),
+      };
+    },
+    head() {
+      return {
+        title: 'Artists',
+      };
+    },
+  };
 </script>
