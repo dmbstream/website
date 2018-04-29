fc702721e925e8cd742fab0cdec5ca75be63ee44
Jim Geurts <jim@biacreations.com>
2018-04-29 11:30:36 -0500

    Redirect to tour if viewing incomplete concert url

--- pages/concerts/_year/_month/index.vue
+++ pages/concerts/_year/_month/index.vue
@@ -1,22 +1,16 @@
 <template>
   <section class="content content-sidebar-right">
-    <div class="card">
-      <div class="card-header">
-        <h2 class="card-title">May 2016</h2>
-      </div>
-      <div class="card-block">
-        <ul>
-          <li><a href="#">2016-01-01 :: Purdue University</a></li>
-          <li><a href="#">2016-01-02 :: The O2</a></li>
-          <li><a href="#">2016-01-06 :: Benneroyal Hall</a></li>
-        </ul>
-      </div>
-    </div>
+    <p>Redirecting to tour</p>
   </section>
 </template>
 
 <script>
   export default {
-    scrollToTop: true
-  }
+    async asyncData({
+      params,
+      redirect,
+                    }) {
+      return redirect(`/tours/${params.year}`)
+    },
+  };
 </script>
