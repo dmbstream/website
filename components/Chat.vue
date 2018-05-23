8d1ebcac1125a6e7f5f8f3b133b3c221b8a1419a
Jim Geurts <jim@biacreations.com>
2018-05-22 22:18:24 -0500

    Initial hook up of chat message api

--- components/Chat.vue
+++ components/Chat.vue
@@ -1,35 +1,57 @@
 <template>
   <div class="card">
     <div class="d-flex h-100 align-items-start flex-column">
-      <a href="#" class="w-100 p-3 breadcrumb">
-        <div class="d-flex w-100 justify-content-between">
-          <h2 class="card-title">Chat</h2>
-          <small>42 online</small>
-        </div>
-      </a>
-      <div class="px-3" style="flex:1" v-scroll-bottom>
-        <template v-for="message in messages">
-          <chat-message :message="message" :key="message.id" />
-        </template>
+      <div class="card-block" v-if="chatLoading">
+        Loading...
       </div>
-      <chat-new-message />
+      <div v-else>
+        <a href="#" class="w-100 p-3 breadcrumb">
+          <div class="d-flex w-100 justify-content-between">
+            <h2 class="card-title">Chat</h2>
+            <small>{{ activeUsers.length }} online</small>
+          </div>
+        </a>
+        <div class="px-3" style="flex:1" v-scroll-bottom>
+          <template v-for="message in messages">
+            <chat-message :message="message" :key="message.id" />
+          </template>
+        </div>
+        <chat-new-message />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import axios from '../plugins/axios';
+
   import ChatMessage from './ChatMessage';
   import ChatNewMessage from './ChatNewMessage';
 
   export default {
-    data() {
-      return {
-        messages: [],
-      };
-    },
     components: {
       ChatMessage,
       ChatNewMessage,
+    },
+    data() {
+      return {
+        stickyMessage: null,
+        activeUsers: [],
+        messages: [],
+        chatLoading: true,
+      };
+    },
+    async mounted() {
+      const chatMessagesResult = await axios.get(`/api/chatmessages`, {
+        params: {
+          includeActiveUsers: true
+        },
+      });
+
+      this.stickyMessage = chatMessagesResult.data.sticky_message;
+      this.activeUsers = chatMessagesResult.data.active_users || [];
+      this.messages = chatMessagesResult.data.items || [];
+      this.chatLoading = false;
     },
     directives: {
       scrollBottom: {
--- components/ChatMessage.vue
+++ components/ChatMessage.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="py-2 chat-row">
     <div class="d-flex w-100 justify-content-between">
-      <nuxt-link :to="{ path: 'user', params: { id: message.user.id } }" class="chat-author">{{ message.user.name }}</nuxt-link>
-      <small>{{ message.sentAt | relativeTime }}</small>
+      <nuxt-link
+        :to="{
+          path: 'users',
+          params: {
+            id: message.created_by.id,
+            slug: slugify(message.created_by.name),
+          }
+        }"
+        class="chat-author">{{ message.created_by.name }}</nuxt-link>
+      <small>{{ message.created_on | relativeTime }}</small>
     </div>
     <div class="pt-1" v-html="body"></div>
   </div>
@@ -10,6 +18,7 @@
 
 <script>
   const moment = require('moment');
+  import stringService from '../plugins/stringService';
 
   const entityMap = {
     '&': '&amp;',
@@ -42,10 +51,13 @@
     },
     computed: {
       body() {
-        const body = escapeHtml(this.message.body);
+        const body = escapeHtml(this.message.text);
         // TODO: Add in emoji rendering
         return body;
       }
+    },
+    methods: {
+      slugify: stringService.slugify
     },
     filters: {
       relativeTime(value) {
